6cb6beb1932ac77982875bbd94b72a52a526afac
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-18 13:57:59 -0700

    add multiple form functionality
    Co-authored-by: Frank Timmons <franktimmonspdx@gmail.com>

--- MadLibs/Views/HomeController.cs
+++ MadLibs/Views/HomeController.cs
@@ -27,12 +27,27 @@ namespace MadLibs.Controllers
       myMadLib.Adjective1 = adjective1;
       myMadLib.Noun1 = noun1;
       myMadLib.Verb = verb;
-      myMadLib.Adjective2 = adjective1;
+      myMadLib.Adjective2 = adjective2;
       myMadLib.Noun2 = noun2;
+      return View(myMadLib);
+    }
+
+    [Route("/ML2")]
+    public ActionResult ML2(string adjective1_2, string noun1_2, string verb_2, string adjective2_2, string noun2_2)
+    {
+      MadLib myMadLib = new MadLib();
+      myMadLib.Adjective1 = adjective1_2;
+      myMadLib.Noun1 = noun1_2;
+      myMadLib.Verb = verb_2;
+      myMadLib.Adjective2 = adjective2_2;
+      myMadLib.Noun2 = noun2_2;
       return View(myMadLib);
     }
 
     [Route("/form")]
     public ActionResult Form() { return View(); }
+
+    [Route("/form2")]
+    public ActionResult Form2() { return View(); }
   }
 }
